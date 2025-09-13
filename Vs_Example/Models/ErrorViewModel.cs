41a4ea5a3cdc5d3820ecba338af2ac0cd39f8d87
ahmedmohsen12340 <ahmedmohsen12340@gmail.com>
2025-09-13 15:58:13 +0300

    Add project files.

---
+++ Vs_Example/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vs_Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
